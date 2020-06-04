bba2d36649aa2d806004525fc19e31647f0fef93
camilo3rd <camilord@abcs.co.nz>
2020-06-04 20:58:48 +1200

    initial commit on java sample project

---
+++ AlphaOneAPISampleProject/src/nz/camilord/alphaone/sample/Main.java
@@ -0,0 +1,8 @@
+package nz.camilord.alphaone.sample;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello AlphaOne API, from Java 11");
+    }
+}
