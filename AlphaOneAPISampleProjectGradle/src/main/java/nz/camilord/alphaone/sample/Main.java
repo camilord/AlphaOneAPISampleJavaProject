0d6d56eff237ba78a9ecf43f4edea23f51eba861
camilo3rd <camilord@abcs.co.nz>
2020-06-06 22:35:35 +1200

    working gradle project + tests

--- AlphaOneAPISampleProjectGradle/src/main/java/nz/camilord/alphaone/sample/Main.java
+++ AlphaOneAPISampleProjectGradle/src/main/java/nz/camilord/alphaone/sample/Main.java
@@ -1,6 +1,17 @@
 package nz.camilord.alphaone.sample;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import nz.alphaone.library.api.Common.AppConfig;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.URL;
+import java.net.URLConnection;
 
 /**
  * AlphaOneAPISampleProject
@@ -26,5 +37,32 @@ public class Main {
         System.out.println("  - " + config.getConfig().get("password"));
 
         System.out.println("\n");
+
+        String sURL = "https://hostgator.abcs.co.nz/submissionget_160907.json"; //just a string
+
+        // Connect to the URL using java's native library
+
+        try {
+            URL url = new URL(sURL);
+            URLConnection request = url.openConnection();
+            request.connect();
+
+            // Convert to a JSON object to print data
+            JsonParser jp = new JsonParser(); //from gson
+            JsonElement root = jp.parse(new InputStreamReader((InputStream) request.getContent())); //Convert the input stream to a json element
+            JsonObject rootobj = root.getAsJsonObject(); //May be an array, may be an object.
+            String zipcode = rootobj.get("submission_guid").getAsString(); //just grab the zipcode
+
+            System.out.println(zipcode);
+
+            JsonArray vrfi = rootobj.get("submission_rfis").getAsJsonArray();
+            System.out.println(vrfi);
+
+            JsonObject prop = rootobj.get("property").getAsJsonObject();
+            System.out.println(prop);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+
 }
