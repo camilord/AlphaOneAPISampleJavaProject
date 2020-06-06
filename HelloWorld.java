749185681d93d86e76382957429d9ce8d6b6288a
camilo3rd <camilord@abcs.co.nz>
2020-06-06 22:48:40 +1200

    working gradle project + tests

---
+++ HelloWorld.java
@@ -0,0 +1,18 @@
+/**
+ * AlphaOneAPISampleProject
+ * Copyright 2020
+ * Generated in IntelliJ IDEA.
+ * Developer: Camilo Lozano III
+ * -> www.camilord.com
+ * -> github.com/camilord
+ * -> linkedin.com/in/camilord
+ * Username: Camilo
+ * Date: 5/06/2020
+ * Time: 8:59 AM
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
